48e7fa3d67a3e53dca711001adda20f2e8580a6d
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-21 01:53:34 +0900

    add: pade workspace

---
+++ ScorerPade/ConvertLog2.cs
@@ -0,0 +1,61 @@
+﻿using MultiPrecision;
+
+namespace ScorerPade {
+    internal class ConvertLog2 {
+        static void Main_() {
+            using (StreamReader sr = new("../../../../results/scorer_hi_n32.csv")) {
+                using StreamWriter sw = new("../../../../results_disused/scorer_hi_n32_log2.csv");
+
+                sw.WriteLine(sr.ReadLine());
+
+                while (!sr.EndOfStream) {
+                    string? line = sr.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line)) {
+                        break;
+                    }
+
+                    string[] line_split = line.Split(',');
+                    string x = line_split[0];
+                    MultiPrecision<Pow2.N32> y = line_split[1];
+
+                    MultiPrecision<Pow2.N32> u = MultiPrecision<Pow2.N32>.Log2(y);
+
+                    sw.WriteLine($"{x},{u}");
+                    Console.WriteLine($"{x},{u:e20}");
+                };
+            }
+
+            using (StreamReader sr = new("../../../../results/scorer_gi_n32.csv")) {
+                using StreamWriter sw = new("../../../../results_disused/scorer_gi_n32_log2.csv");
+
+                sw.WriteLine(sr.ReadLine());
+
+                while (!sr.EndOfStream) {
+                    string? line = sr.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line)) {
+                        break;
+                    }
+
+                    string[] line_split = line.Split(',');
+                    string x = line_split[0];
+
+                    if (x[0] == '-') {
+                        continue;
+                    }
+
+                    MultiPrecision<Pow2.N32> y = line_split[1];
+
+                    MultiPrecision<Pow2.N32> u = MultiPrecision<Pow2.N32>.Log2(y);
+
+                    sw.WriteLine($"{x},{u}");
+                    Console.WriteLine($"{x},{u:e20}");
+                };
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+    }
+}
---
+++ ScorerPade/ScorerHi.cs
@@ -0,0 +1,91 @@
+﻿using MultiPrecision;
+using MultiPrecisionAlgebra;
+using MultiPrecisionCurveFitting;
+
+namespace BarnesGPade {
+    internal class ScorerHi {
+        static void Main_() {
+
+            List<(MultiPrecision<Pow2.N32> x, MultiPrecision<Pow2.N32> y)> expecteds = new();
+
+            using StreamReader sr = new("../../../../results_disused/scorer_hi_n32_log2.csv");
+
+            sr.ReadLine();
+            while (!sr.EndOfStream) {
+                string? line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) {
+                    break;
+                }
+
+                string[] line_split = line.Split(",");
+                MultiPrecision<Pow2.N32> x = line_split[0], y = line_split[1];
+
+                expecteds.Add((-x, y));
+            }
+
+            using StreamWriter sw_result = new("../../../../results_disused/scorer_hi_e32_pade.csv");
+
+            foreach ((int xmin, int xmax) in new[] { (0, 1), (1, 2), (2, 4), (4, 8), (8, 16), (16, 32), (32, 64), (64, 128), (128, 256) }) {
+                List<(MultiPrecision<Pow2.N32> x, MultiPrecision<Pow2.N32> y)> expecteds_range = expecteds.Where(item => item.x >= xmin && item.x <= xmax).ToList();
+
+                MultiPrecision<Pow2.N32> y0 = expecteds_range.Where(item => item.x == xmin).First().y;
+                Vector<Pow2.N32> xs = expecteds_range.Select(item => item.x - xmin).ToArray(), ys = expecteds_range.Select(item => item.y).ToArray();
+
+                for (int m = 4; m <= 32; m++) {
+                    PadeFitter<Pow2.N32> pade = new(xs, ys, m, m, intercept: y0);
+
+                    Vector<Pow2.N32> param = pade.ExecuteFitting();
+                    Vector<Pow2.N32> errs = pade.Error(param);
+
+                    MultiPrecision<Pow2.N32> max_rateerr = 0;
+                    for (int i = 0; i < errs.Dim; i++) {
+                        if (ys[i] == 0) {
+                            continue;
+                        }
+
+                        max_rateerr = MultiPrecision<Pow2.N32>.Max(MultiPrecision<Pow2.N32>.Abs(errs[i] / ys[i]), max_rateerr);
+                    }
+
+                    Console.WriteLine($"m={m},n={m}");
+                    Console.WriteLine($"{max_rateerr:e20}");
+
+                    if (max_rateerr < 2e-32) {
+                        sw_result.WriteLine($"x=[{xmin},{xmax}]");
+                        sw_result.WriteLine($"m={m},n={m}");
+                        sw_result.WriteLine("numer");
+                        foreach (var (_, val) in param[..m]) {
+                            sw_result.WriteLine(val);
+                        }
+                        sw_result.WriteLine("denom");
+                        foreach (var (_, val) in param[m..]) {
+                            sw_result.WriteLine(val);
+                        }
+                        sw_result.WriteLine("hexcode");
+                        for (int i = 0; i < m; i++) {
+                            sw_result.WriteLine($"({ToFP128(param[i])}, {ToFP128(param[i + m])}),");
+                        }
+
+                        sw_result.WriteLine("relative err");
+                        sw_result.WriteLine($"{max_rateerr:e20}");
+                        sw_result.Flush();
+
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+
+        public static string ToFP128(MultiPrecision<Pow2.N32> x) {
+            Sign sign = x.Sign;
+            long exponent = x.Exponent;
+            uint[] mantissa = x.Mantissa.Reverse().ToArray();
+
+            string code = $"({(sign == Sign.Plus ? "+1" : "-1")}, {exponent}, 0x{mantissa[0]:X8}{mantissa[1]:X8}uL, 0x{mantissa[2]:X8}{mantissa[3]:X8}uL)";
+
+            return code;
+        }
+    }
+}
