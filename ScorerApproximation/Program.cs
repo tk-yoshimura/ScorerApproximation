30116fdd43ddf9a91f7d04e5282ad7b7a8e3254f
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-20 09:01:48 +0900

    add: results

--- ScorerApproximation/Program.cs
+++ ScorerApproximation/Program.cs
@@ -3,40 +3,31 @@
 namespace ScorerApproximation {
     internal class Program {
         static void Main() {
-            double x = 113;
-            MultiPrecision<Pow2.N32> ydec = ScorerN32.Gi(MultiPrecision<Pow2.N32>.BitDecrement(x));
-            MultiPrecision<Pow2.N32> y = ScorerN32.Gi(x);
-            MultiPrecision<Pow2.N32> yinc = ScorerN32.Gi(MultiPrecision<Pow2.N32>.BitIncrement(x));
+            using (StreamWriter sw = new("../../../../results/scorer_hi.csv")) {
+                sw.WriteLine("x,scorer_hi(x)");
 
-            Console.WriteLine(ydec);
-            Console.WriteLine(y);
-            Console.WriteLine(yinc);
+                for (double x = -256; x <= 128; x += 1 / 256d) {
+                    MultiPrecision<Pow2.N32> y = ScorerN32.Hi(x);
 
-            //using (StreamWriter sw = new("../../../../results/scorer_hi.csv")) {
-            //    sw.WriteLine("x,scorer_hi(x)");
+                    sw.WriteLine($"{x},{y}");
+                    Console.WriteLine($"{x},{y:e40}");
+                }
 
-            //    for (double x = -256; x <= 256; x += 0.25) {
-            //        MultiPrecision<Pow2.N32> y = ScorerN32.Hi(x);
+                sw.Close();
+            }
 
-            //        sw.WriteLine($"{x},{y}");
-            //        Console.WriteLine($"{x},{y:e40}");
-            //    }
-
-            //    sw.Close();
-            //}
-
-            //using (StreamWriter sw = new("../../../../results/scorer_gi.csv")) {
-            //    sw.WriteLine("x,scorer_gi(x)");
+            using (StreamWriter sw = new("../../../../results/scorer_gi.csv")) {
+                sw.WriteLine("x,scorer_gi(x)");
                 
-            //    for (double x = -32; x <= 256; x += 0.25) {
-            //        MultiPrecision<Pow2.N32> y = ScorerN32.Gi(x);
+                for (double x = -32; x <= 256; x += 1 / 256d) {
+                    MultiPrecision<Pow2.N32> y = ScorerN32.Gi(x);
 
-            //        sw.WriteLine($"{x},{y}");
-            //        Console.WriteLine($"{x},{y:e40}");
-            //    }
+                    sw.WriteLine($"{x},{y}");
+                    Console.WriteLine($"{x},{y:e40}");
+                }
 
-            //    sw.Close();
-            //}
+                sw.Close();
+            }
 
             Console.WriteLine("END");
             Console.Read();
